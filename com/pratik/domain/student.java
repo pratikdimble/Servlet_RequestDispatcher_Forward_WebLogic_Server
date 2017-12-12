000e7dc9e6dc9f1a26360be46c012aafafba5bac
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-12 12:10:50 +0530

    Create student.java

---
+++ com/pratik/domain/student.java
@@ -0,0 +1,28 @@
+package com.pratik.domain;
+
+public class student {
+	public int id;
+	public String name;
+	public String result;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	
+
+}
